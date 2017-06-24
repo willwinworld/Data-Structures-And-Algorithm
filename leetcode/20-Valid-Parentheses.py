c79b05741223909cfe0a585eca6164ea88717174
willwinworld <zhangzhouit@163.com>
2017-06-25 00:02:51 +0800

    c++ static variable

---
+++ leetcode/20-Valid-Parentheses.py
@@ -0,0 +1,13 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+
+def isValid(s):
+    
+
+if __name__ == '__main__':
+    s = 'abc'
+    print(*s)
+    print('a'.isalpha())
+    print('A'.isalpha())
+    print('('.isalpha())
