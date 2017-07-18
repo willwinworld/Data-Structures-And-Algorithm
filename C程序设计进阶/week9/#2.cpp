20b522c12e336c8168330609fdf27940741d90e7
willwinworld <zhangzhouit@163.com>
2017-07-18 23:42:17 +0800

    经典题

--- C程序设计进阶/week9/#2.cpp
+++ C程序设计进阶/week9/#2.cpp
@@ -19,29 +19,52 @@ string convert(){
 	return str;
 }
 
+int k = 0;  // 出现的字母个数 
+struct zimu{  // 用于保存出现的字母及其个数 
+	char letter;
+	int  time;
+};
+
+zimu appearZimu[501];  // 保存所有出现过的字母 
+
+int count(char input){  // 对于字母，进行处理 
+	bool find = false;
+	for (int i = 0; i < k+1; i++){  // 出现过的字母，个数+1 
+		if (input == appearZimu[i].letter){
+			find = true;
+			appearZimu[i].time++;
+			break;
+		}
+	}
+	if (find == false){
+		appearZimu[k].letter = input;
+		appearZimu[k].time = 1;
+		k++;
+	}
+	
+	return 0;
+}
+
 int main()
 {
-	string res = convert();
-	cout << res;
-//	for (int i = 0; str[i] != '\0'; i++){
-//		cout << str[i];
-//	}
-    // 成功转换
+	string after_convert = convert();
+	for (int i=0; after_convert[i] != '\0'; i++){
+		count(after_convert[i]);
+	}
 	
-//	struct Statistics{
-//		char character;
-//		int count;
-//	}; 
-//    
-//    int length = str.length()
-//    Statistics statistics[length];
+	zimu larggest = {' ', 0};
+	zimu second = {' ', 0};
 	
-//	int length = str.length();
-//	int res[length];
-//    
-//    for (int i = 0; str[i] != '\0'; i++){
-//    	if (str[i] ) 
-//    }
-    
+	for (int i = 0; i < k + 1; i++){
+		if (appearZimu[i].time > larggest.time){
+			second = larggest;
+			larggest = appearZimu[i];
+		}
+		else if (appearZimu[i].time < larggest.time && appearZimu[i].time > second.time){
+			second = appearZimu[i];
+		}
+	}
+	
+	cout << second.time;
 	return 0;
 }
