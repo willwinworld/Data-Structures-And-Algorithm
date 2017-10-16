f75d565e15fbb229a490edb26bbfdbef4349123d
willwinworld <zhangzhouit@163.com>
2017-10-16 23:30:43 +0800

    to be continued

---
+++ C++ Prime 5th/chapter1/exercise/Project1/ex1_23.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include "Sales_item.h"
+using namespace std;
+
+
+int main(void) {
+	Sales_item currItem, valItem;
+	if (cin >> currItem) {
+		int cnt = 1;
+		while (cin >> valItem) {
+			if (currItem.isbn() == valItem.isbn()) {
+				++cnt;
+			}
+			else {
+				cout << currItem << "occurs" << cnt << "times" << endl;
+				currItem = valItem;
+				cnt = 1;
+			}
+		}
+		
+		cout << currItem << "occurs" << cnt << "times" << endl;
+	}
+	return 0;
+}
