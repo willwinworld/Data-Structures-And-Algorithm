ded22a12447f6b1e4a136cf729ff148040439bdc
willwinworld <zhangzhouit@163.com>
2017-10-28 13:41:54 +0800

    运算符重载

--- C++程序设计/week4/Project1/Project1/part1.cpp
+++ C++程序设计/week4/Project1/Project1/part1.cpp
@@ -1,25 +1,44 @@
-//#include <iostream>
-//#include <cstring>
-//#include <cstdlib>
-//using namespace std;
-//
-//
-//class Complex {
-//private:
-//	double r, i;
-//public:
-//	void Print() {
-//		cout << r << "+" << i << "i" << endl;
-//	}
-//	// 在此处补充你的代码
-//	Complex operator=(const Complex & a) {
-//
-//    }
-//};
-//
-//int main() {
-//	Complex a;
-//	a = "3+4i"; a.Print();
-//	a = "5+6i"; a.Print();
-//	return 0;
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+
+class Complex {
+private:
+	double r, i;
+public:
+	void Print() {
+		cout << r << "+" << i << "i" << endl;
+	}
+	// 在此处补充你的代码
+	// 构造函数
+	Complex(double real = 0.0, double imaginary = 0.0) {
+		r = real;  // real part
+		i = imaginary;  // imaginary part
+	}
+	Complex & operator=(string s) {
+		int position = s.find("+", 0);
+		string firstPart = s.substr(0, position);  // substr参数含义：第一个参数是开始位置，第二个参数是字符串的长度，而不是字符串结束位置
+		string secondPart = s.substr(position + 1, s.length() - position - 2);
+		r = atof(firstPart.c_str());
+		i = atof(secondPart.c_str());
+		return *this;
+	}
+	/*Complex(double real = 0.0, double imaginary = 0.0):
+		r(real), i(imaginary){}
+	Complex operator=(const Complex &);*/
+};
+
+//Complex Complex::operator=(const Complex & operand2) {
+//	return Complex(r = operand2.r,
+//		           i = operand2.i);
 //}
+
+int main() {
+	Complex a;
+	a = "3+4i"; a.Print();
+	a = "5+6i"; a.Print();
+	getchar();
+	return 0;
+}
--- C++程序设计/week4/Project1/Project1/part1_example2.cpp
+++ C++程序设计/week4/Project1/Project1/part1_example2.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-
-
-/*
-运算符重载为成员函数
-重载为成员函数时，参数个数为运算符目数减一
-*/
-class Complex {
-public:
-	Complex(double r=0.0, double m=0.0):
-		real(r), imaginary(m){}   // constructor
-	Complex operator+(const Complex &); // addition
-	Complex operator-(const Complex &); // subtraction
-private:
-	double real;  // real part
-	double imaginary;  // imaginary part
-};
-
-// overloaded addition operator
-Complex Complex::operator+(const Complex & operand2) {
-	return Complex(real + operand2.real,
-		imaginary + operand2.imaginary);
-}
-// overloaded subtraction operator
-Complex Complex::operator-(const Complex & operand2) {
-	return Complex(real - operand2.real,
-		imaginary - operand2.imaginary);
-}
-
-int main() {
-	Complex x, y(4.3, 8.2), z(3.3, 1.1);
-	x = y + z;
-	cout << x.operator+ << endl;
-	return 0;
-}	
+//#include <iostream>
+//#include <stdio.h>
+//using namespace std;
+//
+//
+///*
+//运算符重载为成员函数
+//重载为成员函数时，参数个数为运算符目数减一
+//*/
+//class Complex {
+//public:
+//	Complex(double r=0.0, double m=0.0):
+//		real(r), imaginary(m){}   // constructor
+//	Complex operator+(const Complex &); // addition
+//	Complex operator-(const Complex &); // subtraction
+//private:
+//	double real;  // real part
+//	double imaginary;  // imaginary part
+//};
+//
+//// overloaded addition operator
+//Complex Complex::operator+(const Complex & operand2) {
+//	return Complex(real + operand2.real,
+//		imaginary + operand2.imaginary);
+//}
+//// overloaded subtraction operator
+//Complex Complex::operator-(const Complex & operand2) {
+//	return Complex(real - operand2.real,
+//		imaginary - operand2.imaginary);
+//}
+//
+//int main() {
+//	Complex x, y(4.3, 8.2), z(3.3, 1.1);
+//	x = y + z;  // x = y.operator + z
+//	x = y - z;  // x = y.operator - z
+//	return 0;
+//}	
