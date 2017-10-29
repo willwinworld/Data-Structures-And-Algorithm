bd7eb4cdb9a8995ab5a9283d36edb502519505c6
willwinworld <zhangzhouit@163.com>
2017-10-29 23:10:31 +0800

    二维数组类

--- C++程序设计/week4/Project1/Project1/part2.cpp
+++ C++程序设计/week4/Project1/Project1/part2.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
-class MyInt {
-	int nVal;
-public:
-	MyInt(int n) { nVal = n; }
-	int ReturnVal() { return nVal; }
-	// 在此处补充你的代码
-	MyInt & operator -(int i) {
-		nVal -= i;
-		return *this;
-	}
-};
-int main() {
-	/*输出4 1*/
-	MyInt objInt(10);
-	objInt - 2 - 1 - 3;
-	cout << objInt.ReturnVal();
-	cout << ",";
-	objInt - 2 - 1;
-	cout << objInt.ReturnVal();
-	getchar();
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//class MyInt {
+//	int nVal;
+//public:
+//	MyInt(int n) { nVal = n; }
+//	int ReturnVal() { return nVal; }
+//	// 在此处补充你的代码
+//	MyInt & operator -(int i) {
+//		nVal -= i;
+//		return *this;
+//	}
+//};
+//int main() {
+//	/*输出4 1*/
+//	MyInt objInt(10);
+//	objInt - 2 - 1 - 3;
+//	cout << objInt.ReturnVal();
+//	cout << ",";
+//	objInt - 2 - 1;
+//	cout << objInt.ReturnVal();
+//	getchar();
+//	return 0;
+//}
--- C++程序设计/week4/Project1/Project1/part3.cpp
+++ C++程序设计/week4/Project1/Project1/part3.cpp
@@ -11,20 +11,57 @@ next
 0,1,2,3,
 4,5,6,7,
 8,9,10,11,
+
+memcpy(destination, source, number_of_bytes)
+
+int *array = new int[n];
+
+new allocates an amount of memory needed to store the object/array that you request.
+In this case n numbers of int.
+The pointer will then store the address to this block of memory.
+But be careful, this allocated block of memory will not be freed until you tell it by writing.
+
+delete [] array
+*/
+/*
+要重载操作符'[]',支持二维数组下标
+要重载操作符 '()'
+要进行深度拷贝,建立自己的拷贝构造函数
 */
 class Array2 {
 private:
-	int * a;
-	int row, col;
+	int row;  // 二维数组的行
+	int column;  // 二维数组的列
+	int *ptr; // 指向二维数组的指针
 public:
-	Array2() { a = NULL; }
-	Array2(int i_, int j_) {
-		i = i_;
-		j = j_;
-		a = new int[i*j];
+	Array2() {  // 构造函数
+		ptr = NULL;
 	}
-	Array2(Array2 &t) {
-
+	Array2(int paraRow, int paraColumn):row(paraRow), column(paraColumn) {  
+		ptr = new int[row * column];  // 构造函数
+	}
+	Array2(Array2& a):row(a.row), column(a.column) {  // 深度拷贝
+		ptr = new int[row * column];
+		memcpy(ptr, a.ptr, sizeof(int)*row*column);
+	}
+	Array2& operator= (const Array2 &a) {  // 重载=
+		if (ptr)
+			delete[]ptr;
+		row = a.row;
+		column = a.column;
+		ptr = new int[row*column];
+		memcpy(ptr, a.ptr, sizeof(int)*row*column);
+		return *this;
+	}
+	~Array2() {  // 析构函数
+		if (ptr)
+			delete[] ptr;
+	}
+	int* operator [] (int i) {
+		return ptr + i * column; // 重载的实际上是第二维的[],第一维的[]直接调用int型一维数组的定义
+	}
+	int& operator () (int i, int j) {
+		return ptr[i*column + j];  // 重载操作符()
 	}
 };
 
@@ -49,5 +86,6 @@ int main() {
 		}
 		cout << endl;
 	}
+	getchar();
 	return 0;
 }
